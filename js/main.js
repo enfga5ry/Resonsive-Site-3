89ab12480357334161d2e6a7bbf31d415cad2c35
Mohamed Fakhry <Muhammadfakhry2020@gmail.com>
2022-10-17 20:39:54 +0200

    git commit -m "nice"

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let skillsSection = document.querySelector(".our-skills");
 let skillsMiniBox = document.querySelectorAll(".skills .skill h3 span");
 let skillsSpan = document.querySelectorAll(".skills .skill .progress span");
 
-window.onscroll = function () {
+function first() {
   if (window.scrollY >= skillsSection.offsetTop) {
     skillsSpan.forEach((e) => {
       e.style.width = e.dataset.complete;
@@ -14,21 +14,22 @@ window.onscroll = function () {
         ele.parentElement.nextElementSibling.firstElementChild.dataset.complete;
     });
   }
-};
+}
 
 //  states
 
 let statsSection = document.querySelector(".stats");
 let boxes = document.querySelectorAll(".stats .box .number");
 let starting = false;
-window.onscroll = function () {
+
+function second() {
   if (window.scrollY >= statsSection.offsetTop) {
     if (!starting) {
       boxes.forEach((box) => counter(box));
     }
     starting = true;
   }
-};
+}
 
 function counter(box) {
   let goals = box.dataset.goal;
@@ -40,3 +41,8 @@ function counter(box) {
     }
   }, 2000 / goals);
 }
+
+window.onscroll = function () {
+  first();
+  second();
+};
